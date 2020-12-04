a14f7d1124984525e644629bb83bb5f616eeb5e9
Cyril Leclerc <cyril@dashlane.com>
2020-12-04 11:49:19 +0100

    break everything

--- buildroot-external/package/hassio/builder/hostapp.sh
+++ buildroot-external/package/hassio/builder/hostapp.sh
@@ -26,20 +26,20 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-SUPERVISOR="homeassistant/${ARCH}-hassio-supervisor"
-DNS="homeassistant/${ARCH}-hassio-dns"
-AUDIO="homeassistant/${ARCH}-hassio-audio"
-CLI="homeassistant/${ARCH}-hassio-cli"
-MULTICAST="homeassistant/${ARCH}-hassio-multicast"
-OBSERVER="homeassistant/${ARCH}-hassio-observer"
-LANDINGPAGE="homeassistant/${MACHINE}-homeassistant:landingpage"
+SUPERVISOR="b46ha/${ARCH}-hassio-supervisor"
+#DNS="homeassistant/${ARCH}-hassio-dns"
+#AUDIO="homeassistant/${ARCH}-hassio-audio"
+#CLI="homeassistant/${ARCH}-hassio-cli"
+#MULTICAST="homeassistant/${ARCH}-hassio-multicast"
+#OBSERVER="homeassistant/${ARCH}-hassio-observer"
+LANDINGPAGE="b46ha/${MACHINE}-homeassistant:landingpage"
 
 SUPERVISOR_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.supervisor')
-DNS_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.dns')
-CLI_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.cli')
-AUDIO_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.audio')
-MULTICAST_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.multicast')
-OBSERVER_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.observer')
+# DNS_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.dns')
+# CLI_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.cli')
+# AUDIO_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.audio')
+# MULTICAST_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.multicast')
+# OBSERVER_VERSION=$(curl -s ${VERSION_URL} | jq -e -r '.observer')
 
 # Make image
 dd if=/dev/zero of=${DATA_IMG} bs=1G count=1
@@ -76,11 +76,11 @@ docker pull "${SUPERVISOR}:${SUPERVISOR_VERSION}"
 docker tag "${SUPERVISOR}:${SUPERVISOR_VERSION}" "${SUPERVISOR}:latest"
 
 # Install Plugins
-docker pull "${CLI}:${CLI_VERSION}"
-docker pull "${DNS}:${DNS_VERSION}"
-docker pull "${AUDIO}:${AUDIO_VERSION}"
-docker pull "${MULTICAST}:${MULTICAST_VERSION}"
-docker pull "${OBSERVER}:${OBSERVER_VERSION}"
+# docker pull "${CLI}:${CLI_VERSION}"
+# docker pull "${DNS}:${DNS_VERSION}"
+# docker pull "${AUDIO}:${AUDIO_VERSION}"
+# docker pull "${MULTICAST}:${MULTICAST_VERSION}"
+# docker pull "${OBSERVER}:${OBSERVER_VERSION}"
 
 # Install landing page
 docker pull "${LANDINGPAGE}"
